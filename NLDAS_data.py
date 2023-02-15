79d6d1d5051a943afa5b6c9d7cdefe2ff285b035
bolotinl <bolotinljb@gmail.com>
2023-02-15 13:29:01 -0800

    Extract watershed areas for later use

--- NLDAS_data.py
+++ NLDAS_data.py
@@ -27,3 +27,11 @@ sp.out_dir = '../San Pedro River/NLDAS_climrods/'
 
 weight_dir = '../San Pedro River/NLDAS_climrods/weighted_P/'
 sp.area_weight_P(weight_dir)
+
+sp.intsct.to_csv('../San Pedro River/NLDAS_intsct.csv')
+
+# # Somehow within the climrods code, extract the following info:
+# watershed_file = watershed
+# watershed_file = pd.DataFrame(watershed.drop(columns='geometry'))
+# watershed_file['AREA_km2']=watershed_area
+# watershed_file.to_csv('/Volumes/GoogleDrive/Shared drives/Lauren and Hilary/Regional Overland Flow Project/Data and Scripts/San Pedro River/watershed_areas.csv')
