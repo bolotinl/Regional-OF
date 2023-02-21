7cdd9645a6044363c8af156607e7fb0e2c5798ca
bolotinl <bolotinljb@gmail.com>
2023-02-21 15:39:31 -0800

    Re-calculate with area-weighted NLDAS precip

--- San_Pedro_Monsoon_Sigs.m
+++ San_Pedro_Monsoon_Sigs.m
@@ -7,7 +7,7 @@ clc
 mydir = pwd;
 
 %%
-cd ../../../../../'Shared drives'/'Lauren and Hilary'/'Regional Overland Flow Project'/'Data and Scripts'/'San Pedro River'/Monsoon_Data/
+cd '/Volumes/GoogleDrive/Shared drives/Lauren and Hilary/Regional Overland Flow Project/Data and Scripts/San Pedro River/Monsoon_Data'
 
 %% prepare for OF signatures
 addpath(genpath('/Volumes/GoogleDrive/My Drive/Overland Flow MS/TOSSH-master/TOSSH_code'))
@@ -20,12 +20,11 @@ addpath(genpath('/Volumes/GoogleDrive/Shared drives/Lauren and Hilary/Data from 
 files = dir(fullfile(pwd, '*uv.csv'));
 
 % Create consistent components of output filepaths
-folder_name = '../Signature Data/';
+folder_name = '../Signature Data/area_weighted_NLDAS/';
 file_name_end_total = '_uv_results_monsoon.csv';
 
-% run i = 17 manually and change line 33 column name
-% 16 is weird for Wu
-for i = 18:length(files)
+% Can't figure out why 16 doesn't work -- skip it and run i = 17 manually
+for i = 1:length(files)
     data = readtable(files(i).name);    
     site8 = files(i).name(1:8);
     site = str2double(site8);
@@ -56,11 +55,11 @@ end
 files = dir(fullfile(pwd, '*dv.csv'));
 
 % Create consistent components of output filepaths
-folder_name = '../Signature Data/';
+folder_name = '../Signature Data/area_weighted_NLDAS/';
 file_name_end_total = '_dv_results_monsoon.csv';
 
-
-for i = 3:length(files)
+% files 1-3 have no events or data during the monsoon season
+for i = 4:length(files)
     data = readtable(files(i).name);    
     site8 = files(i).name(1:8);
     site = str2double(site8);
@@ -94,11 +93,11 @@ cd ../QP_Data/
 files = dir(fullfile(pwd, '*uv.csv'));
 
 % Create consistent components of output filepaths
-folder_name = '../Signature Data/';
+folder_name = '../Signature Data/area_weighted_NLDAS/';
 file_name_end_total = '_uv_results_all.csv';
 
 % i = 16 isn't working
-for i = 1:length(files)
+for i = 17:length(files)
     data = readtable(files(i).name);    
     site8 = files(i).name(1:8);
     site = str2double(site8);
@@ -127,11 +126,11 @@ end
 files = dir(fullfile(pwd, '*dv.csv'));
 
 % Create consistent components of output filepaths
-folder_name = '../Signature Data/';
+folder_name = '../Signature Data/area_weighted_NLDAS/';
 file_name_end_total = '_dv_results_all.csv';
 
 
-for i = 1:length(files)
+for i = 4:length(files)
     data = readtable(files(i).name);    
     site8 = files(i).name(1:8);
     site = str2double(site8);
